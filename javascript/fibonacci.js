316a1feec1ed84c97849a746932f2d76e633c3a6
Phyleria <atienophylis032@gmail.com>
2019-04-08 06:00:10 -0400

    fibonacci sequence

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -5,8 +5,8 @@ function* fibonacci(){
         var current = fn1;
         fn1 = fn2;
         fn2 = current +fn1;
-        var reset = yield current;
-        if (reset){
+        var reset = yield current;e
+        if (reset){a
             fn1 = 0;
             fn2 = 1;
         }
